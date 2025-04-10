cc23374dfde32e06e424ef10e75ee43e77a41999
YordanovDY <yordanovdy23@gmail.com>
2025-04-10 20:52:53 +0300

    Fix year change bug

--- src/components/providers/ScheduleProvider.tsx
+++ src/components/providers/ScheduleProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useReducer } from "react";
 import { ScheduleContext } from "../../contexts/ScheduleContext";
 import { Month } from "../../types/MonthType";
 
@@ -9,33 +9,49 @@ interface ScheduleProviderProps {
 export default function ScheduleProvider({ children }: ScheduleProviderProps) {
     const now = new Date();
 
-    const [month, setMonth] = useState<Month>(now.getMonth() + 1 as Month);
-    const [year, setYear] = useState<number>(now.getFullYear());
+    interface DateState {
+        month: Month,
+        year: number
+    }
+
+    type DateAction = { type: 'NEXT_MONTH' } | { type: 'PREVIOUS_MONTH' };
+
+    const dateReducer = (state: DateState, action: DateAction): DateState => {
+        switch (action.type) {
+            case 'PREVIOUS_MONTH':
+                if (state.month === 1) {
+                    return { month: 12, year: state.year - 1 }
+                }
+
+                return { month: state.month - 1 as Month, year: state.year }
+
+            case 'NEXT_MONTH':
+                if (state.month === 12) {
+                    return { month: 1, year: state.year + 1 }
+                }
+
+                return { month: state.month + 1 as Month, year: state.year };
+
+
+            default:
+                return state;
+        }
+    }
+
+    const [date, dispatch] = useReducer(dateReducer, { month: now.getMonth() + 1 as Month, year: now.getFullYear() });
 
     const previousMonth = (): void => {
-        setMonth(state => {
-            if (state === 1) {
-                setYear(y => y - 1);
-                return 12;
-            }
-
-            return state - 1 as Month;
-        });
+        dispatch({ type: 'PREVIOUS_MONTH' });
     }
 
     const nextMonth = (): void => {
-        setMonth(state => {
-            if (state === 12) {
-                setYear(y => y + 1);
-                return 1;
-            }
-
-            return state + 1 as Month;
-        });
+        dispatch({ type: 'NEXT_MONTH' });
     }
 
+    const { month, year } = date;
+
     return (
-        <ScheduleContext.Provider value={{month, year, previousMonth, nextMonth}}>
+        <ScheduleContext.Provider value={{ month, year, previousMonth, nextMonth }}>
             {children}
         </ScheduleContext.Provider>
     );
